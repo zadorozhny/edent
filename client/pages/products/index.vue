2837df8d4f2c61d53b04297d6477290ab7078901
Artem Zadorozhnyi <artemzadorozhnyi@MacBook-OC-105363.local>
2019-10-12 17:10:16 +0300

    test

--- client/pages/products/index.vue
+++ client/pages/products/index.vue
@@ -37,7 +37,6 @@ export default {
     return {
       option: 'ex',
       value: 0,
-      test: 0,
       cards: [
         { title: 'Товар 1', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg' },
         { title: 'Товар 2', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
