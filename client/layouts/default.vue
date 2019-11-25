cb2d6d23104a909335fc8c56f774b8df25051cf5
Artem Zadorozhnyi <a.zadorozhnyi@mobidev.biz>
2019-11-26 00:05:49 +0200

    add styles for product page

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -47,7 +47,7 @@ export default {
 }
 
 .search {
-  height: 40px;
+  height: 36px;
   width: 340px;
 
   @media ($tablet) {
--- client/pages/products/_id/index.vue
+++ client/pages/products/_id/index.vue
@@ -1,47 +1,32 @@
 <template>
   <section class="page container">
     <div class="product">
-      <div class="picture">
+      <div class="product--picture">
         <img :src="value">
       </div>
-      <div class="content">
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
-        <p>Lorem Ipsum</p>
+      <div class="product--content">
+        <div class="product--head">
+          <h2 class="text_large">Даймондбрайт</h2>
+          <span class="text_medium">#132</span>
+        </div>
+        <div class="product--info">
+          <p class="text_medium">Модель: CKD000340</p>
+          <p class="text_medium">Производитель: Cerkamed</p>
+        </div>
+        <p class="product--price text_title">Цена: <span class="text_large">1299</span></p>
         <div class="buttons">
           <kit-button type="success" class="button">Купить</kit-button>
           <kit-button class="button">В корзину</kit-button>
         </div>
       </div>
+      <p class="product--description text_medium">
+        <span class="text_bold">Описание:</span> Сайт рыбатекст поможет дизайнеру, верстальщику,
+        вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке,
+        а начинающему оратору отточить навык публичных выступлений в домашних условиях.
+        При создании генератора мы использовали небезизвестный универсальный код речей.
+        Текст генерируется абзацами случайным образом от двух до десяти предложений в абзаце,
+        что позволяет сделать текст более привлекательным и живым для визуально-слухового восприятия.
+      </p>
     </div>
   </section>
 </template>
@@ -56,20 +41,75 @@ export default {
 
 <style lang="scss" scoped>
 .product {
-  display: flex;
-  align-items: flex-start;
+  display: grid;
+  grid-template-columns: 50% calc(50% - 20px);
+  grid-template-areas:
+    "picture content"
+    "description description";
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
 
   @media ($phablet) {
-    flex-direction: column;
-    margin-bottom: 44px;
+    margin-bottom: 54px;
+    grid-template-columns: 100%;
+    grid-template-rows: auto 200px auto;
+    grid-column-gap: unset;
+    grid-row-gap: 10px;
+    grid-template-areas:
+      "picture picture"
+      "content content"
+      "description description";
   }
-}
 
-.content {
-  width: 50%;
+  &--picture {
+    margin-right: 20px;
+    width: 100%;
+    grid-area: picture;
+    box-shadow: 0px 1px 4px 0 rgba(0,0,0,0.3);
+    border-radius: 3px;
+    overflow: hidden;
 
-  @media ($phablet) {
-    padding: 10px;
+    @media ($phablet) {
+      width: 100%;
+      border-radius: 0px;
+    }
+
+    img {
+      display: block;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+
+  &--content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    grid-area: content;
+    box-sizing: border-box;
+    width: 100%;
+
+    @media ($phablet) {
+      padding: 10px;
+    }
+  }
+
+  &--description {
+    grid-area: description;
+
+    @media ($phablet) {
+      padding: 10px;
+    }
+  }
+
+  &--info {
+    display: grid;
+    grid-row-gap: 10px;
+  }
+
+  &--head {
+    display: flex;
+    justify-content: space-between;
   }
 }
 
@@ -96,25 +136,6 @@ export default {
     &:first-child {
       margin-right: 0;
     }
-  }
-}
-
-.picture {
-  margin-right: 20px;
-  width: 50%;
-  box-shadow: 0px 1px 4px 0 rgba(0,0,0,0.3);
-  border-radius: 3px;
-  overflow: hidden;
-
-  @media ($phablet) {
-    width: 100%;
-    border-radius: 0px;
-  }
-
-  img {
-    display: block;
-    width: 100%;
-    object-fit: cover;
   }
 }
 </style>
