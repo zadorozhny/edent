4b13f3b1067baa861b8fc90d7eef4bffb1e8eff9
Artem Zadorozhnyi <a.zadorozhnyi@mobidev.biz>
2019-12-16 17:54:03 +0200

    add logic for profile

--- server/services/profile/Utility.js
+++ server/services/profile/Utility.js
@@ -1,11 +1,9 @@
-import { models, sequelize } from '@/database';
-import { utility as schemas } from '@/services/profile/schemas';
-import { ERRORS } from '@/config';
-import { service, schema } from '@/lib/services/decorators';
-import { noreply } from '@/lib/Mailer';
-import { AccessToken, RefreshToken, ConfirmationToken, ResetToken } from '@/lib/Tokens';
+import { models } from '@/database';
+import ServiceError from '@/lib/Errors';
+import { service } from '@/lib/services/decorators';
+import { AccessToken, RefreshToken } from '@/lib/Tokens';
 
-@service({ schemas })
+@service
 export default class Utility {
   static createTokens(user) {
     return {
@@ -20,64 +18,27 @@ export default class Utility {
     };
   }
   async signup(data) {
-    const transaction = await sequelize.transaction();
-    try {
-      const user = await models.User.create(data);
-      return user.tokens(this.constructor.createTokens(user));
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
+    const user = await models.User.create(data);
+    return user.tokens(this.constructor.createTokens(user));
   }
   async signin({ email, password }) {
     const user = await models.User.findOne({ where: { email } });
     if (!user) {
-      throw HttpError(404, ERRORS.USER_NOT_FOUND);
+      throw ServiceError(404, 'ERRORS.USER_NOT_FOUND');
     }
     if (!user.isCurrentPassword(password)) {
-      throw HttpError(400, ERRORS.WRONG_PASSWORD);
+      throw ServiceError(400, 'ERRORS.WRONG_PASSWORD');
     }
     return user.tokens(this.constructor.createTokens(user));
   }
   async signout() {
     return RefreshToken.destroy(this.user.id);
   }
-  @schema('refresh')
   async refresh({ refreshToken }) {
     if (await RefreshToken.verify(this.user.id, refreshToken)) {
       return this.constructor.createTokens(this.user);
     } else {
-      throw HttpError(401, ERRORS.WRONG_REFRESH_TOKEN);
-    }
-  }
-  @schema('forgotPassword')
-  async forgotPassword({ email }) {
-    const user = await models.User.findOne({
-      where: { email }
-    });
-    if (!user) {
-      throw HttpError(404, ERRORS.USER_NOT_FOUND);
-    }
-    noreply.send({
-      to: user.email,
-      type: 'reset-password',
-      data: {
-        token: ResetToken.create({ userId: user.id })
-      }
-    });
-  }
-  @schema('resetPassword')
-  async resetPassword({ resetToken, password }) {
-    try {
-      const { userId } = ResetToken.verify(resetToken);
-      await models.User.update({ password }, {
-        where: { id: userId }
-      });
-    } catch (err) {
-      if (err.name === 'TokenExpiredError') {
-        throw HttpError(400, ERRORS.RESET_TOKEN_EXPIRED);
-      }
-      throw HttpError(400, ERRORS.WRONG_RESET_TOKEN);
+      throw ServiceError(401, 'ERRORS.WRONG_REFRESH_TOKEN');
     }
   }
 }
